0fa56ce7ad3136eacffc0ca06bd5a48301854596
FootballNewsroom <wabwilesimiyu@gmail.com>
2023-06-26 15:54:16 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints the second half of a string followed by a new line.
+ * @str: The input string.
+ */
+void puts_half(char *str)
+{
+  int length = 0;
+  int i, start_index;
+
+  while (str[length] != '\0')
+    length++;
+
+  if (length % 2 == 0)
+    start_index = length / 2;
+  else
+    start_index = (length + 1) / 2;
+
+  for (i = start_index; i < length; i++)
+    _putchar(str[i]);
+
+  _putchar('\n');
+}
+
