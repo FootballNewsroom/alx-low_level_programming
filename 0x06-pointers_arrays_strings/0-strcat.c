e3be12cd367ad1671bb27ddcadd86b39a71497be
FootballNewsroom <wabwilesimiyu@gmail.com>
2023-06-29 05:18:28 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,29 +1,23 @@
 #include "main.h"
 
 /**
- * _strcat - concatenates two strings
- * @dest: input value
- * @src: input value
- * Return: void
+ * _strcat - concatenates two strings.
+ * @dest: destination string pointer.
+ * @src: source string pointer.
+ * Return: pointer to destination string.
  */
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
-
-	i = 0;
-	while (dest[i] != '\0')
+	int length_of_string, z;
+	length_of_string = 0;
+	while (dest[length_of_string] != '\0')
 	{
-		i++;
+		length_of_string++;
 	}
-	j = 0;
-	while (src[j] != '\0')
+	for (z = 0; src[z] != '\0'; z++, length_of_string++)
 	{
-		dest[i] = src[j];
-		i++;
-		j++;
+		dest[length_of_string] = src[z];
 	}
-
-	dest[i] - '\0';
+	dest[length_of_string] = '\0';
 	return (dest);
 }
