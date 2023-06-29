2c272cd80b719ba887abea619f64d6c8065a8619
FootballNewsroom <wabwilesimiyu@gmail.com>
2023-06-29 08:04:26 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,7 +9,7 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-        int length_of_string, z;
+       	int length_of_string, z;
 /* z is a counter for n bytes of src to be concatenated */
 /* length_of_string = length of destination string */
 
@@ -18,7 +18,7 @@ char *_strncat(char *dest, char *src, int n)
 	{
 		length_of_string++;
 	}
-	for (z = 0; z < n &&src[z] != '\0'; z++, length_of_string++)
+       	for (z = 0; z < n && src[z] != '\0'; z++, length_of_string++)
 	{
 		dest[length_of_string] = src[z];
 	}
