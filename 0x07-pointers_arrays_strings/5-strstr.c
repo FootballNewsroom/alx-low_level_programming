2de2f3525f0295dcea98ca21a237a9f0b9d672d3
FootballNewsroom <wabwilesimiyu@gmail.com>
2023-07-03 13:48:55 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,7 +4,8 @@
  * @haystack: the string to be searched.
  * @needle: the substring to be located.
  *
- * Return: if the substring is located - a pointer to the beginning of the located substring.
+ * Return: if the substring is located - a pointer to the beginning
+ * of the located substring.
  * if the substring is not located - NULL.
  */
 char *_strstr(char *haystack, char *needle)
