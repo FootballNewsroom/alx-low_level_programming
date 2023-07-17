0cc7b269734e73a0db5464db8fcc1ff7d6b3dbe2
FootballNewsroom <wabwilesimiyu@gmail.com>
2023-07-17 14:57:17 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * stuct dog - represents a dog.
+ * @name: name of a dog.
+ * @owner: the dog's owner.
+ * @age: age of the dog in years.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
